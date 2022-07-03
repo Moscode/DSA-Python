309ad764daaf7480b029295b8cce602cd2ece481
Odunosh Moses <Odunoshomoses@gmail.com>
2022-07-03 22:44:39 +0000

    [feat]: made a recursive call

--- Permutation_Backtracking.py
+++ Permutation_Backtracking.py
@@ -3,4 +3,10 @@ class Solution:
         solution = []
         #Base Case
         if (len(nums) == 1):
-            
+            return [nums[:]]
+        
+        for index in range(len(nums)):
+            num = nums.pop(0)
+            permutate = permutation(nums)
+        
+        
