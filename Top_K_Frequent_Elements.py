4d2d8679d9519fee5c15fa23bd495c40ec8a86f4
Odunosh Moses <Odunoshomoses@gmail.com>
2022-07-22 13:15:04 +0000

    [fix]: added concept I lesarnt in this problem

--- Top_K_Frequent_Elements.py
+++ Top_K_Frequent_Elements.py
@@ -1,5 +1,11 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        """
+        (1) Learnt how to sort a dictionary using values or keys
+        (2) learnt that even though dictionary keys return a list it can't still be slice 
+        because it is still diction until when list method is used on it
+        (3) Understanding how to use slice with negative values better
+        """
         hashmap = {}
         for word in nums:
             if word in hashmap:
