b0c28c01309b1a5eed1dff07ecedc7bd7c5b5ae4
Odunosh Moses <Odunoshomoses@gmail.com>
2022-07-24 06:59:09 +0000

    [fix]: added another solution with O(1) space

--- Product_Of_Array_Except_Self.py
+++ Product_Of_Array_Except_Self.py
@@ -1,5 +1,25 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """
+        time complexity: O(n), space complexity: O(1) since the question ignore the output/res space
+        pref = 1
+        postfix = 1
+        res = [1 for i in range(len(nums))]
+        
+        for i in range(len(nums)):
+            res[i] = pref
+            pref *= nums[i]
+            
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+            
+        return res
+        """
+        
+        
+        """
+        time complexity: O(n), space complexity: O(n)
         prefix = [1 for i in nums]
         postfix = [1 for i in nums]
         
@@ -12,3 +32,4 @@ class Solution:
             postfix[i] = postfix[i + 1] * nums[i + 1]
             
         return [prefix[i] * postfix[i] for i in range(len(nums))]
+        """
