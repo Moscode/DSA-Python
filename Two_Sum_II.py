e1b3b655c89d4e7563a4a880cc52b4949759e960
Odunosh Moses <Odunoshomoses@gmail.com>
2022-08-05 08:57:11 +0000

    [fix]: what I learnt added as comment

--- Two_Sum_II.py
+++ Two_Sum_II.py
@@ -1,4 +1,9 @@
 class Solution:
+    """
+    What I Learnt:
+    Two pointers is used on string, array, and linkedlist when searching and camparing is needed.
+    While loop is used to monitor the pointers and indices are bettered assigned to a variable
+    """
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         first_i = 0
         back_i = len(numbers) - 1
