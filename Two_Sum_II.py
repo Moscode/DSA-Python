3bca3d62b6b77206b703dc815ac53a555745dd11
Odunosh Moses <Odunoshomoses@gmail.com>
2022-08-05 08:58:56 +0000

    [fix]: added time and space complexity

--- Two_Sum_II.py
+++ Two_Sum_II.py
@@ -4,6 +4,7 @@ class Solution:
     Two pointers is used on string, array, and linkedlist when searching and camparing is needed.
     While loop is used to monitor the pointers and indices are bettered assigned to a variable
     """
+    # Time Complexity: O(n) and Space Complexity: O(1)
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         first_i = 0
         back_i = len(numbers) - 1
