12f111d77b18c73a2b4a72b54904e84b9d88c11a
Odunosh Moses <Odunoshomoses@gmail.com>
2022-08-20 16:35:54 +0000

    moving on from exercise will be back later

---
+++ EPI-One-Month-Prep/Primitive-Types/4.3_reversing_value.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+
+"""
+Should be solved using bitwise knowledge
+def reverse_value(N):
+    Arr = []
+    i = (len(N) - 1)
+    while i > -1:
+        Arr.append(N[i])
+        i -= 1
+    return Arr
+
+solution = reverse_value("1110000000000001")
+string_solution = ''.join(str(n) for n in solution)
+print(string_solution)
+"""
